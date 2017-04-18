710984438f0603aaceeb900a5b81279932514780
czbiao <13413423055@163.com>
2017-04-18 21:07:24 +0800

    4.18社区页面添加类似朋友圈

---
+++ app/src/main/java/com/example/biao/thehealth/commun/circledemo/listener/RecycleViewItemListener.java
@@ -0,0 +1,10 @@
+package com.example.biao.thehealth.commun.circledemo.listener;
+
+/**
+ * Created by yiwei on 16/4/9.
+ */
+public interface RecycleViewItemListener {
+
+    void onItemClick(int position);
+    boolean onItemLongClick(int position);
+}
